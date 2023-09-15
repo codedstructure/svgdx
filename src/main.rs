37b46fa1d81a5bc5158992e684a45fe62744b507
Rich Clubb <rich.clubb@gmail.com>
2023-09-15 11:12:52 +0100

    basic command line input, only input is currently working

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
+use clap::Parser;
+use std::fs;
+
 use svgd::Transformer;
 
+fn path_exists(path: &str) -> bool {
+    fs::metadata(path).is_ok()
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about=None)]
+struct Args {
+    #[arg(short, long)]
+    input_file_path: String,
+
+    #[arg(short, long)]
+    output_file_path: Option<String>,
+}
+
 fn main() {
-    let mut t = Transformer::new("boxes-tbox.svg");
-    t.transform();
+    let args = Args::parse();
+
+    if path_exists(&args.input_file_path) == false {
+        panic!("File does not exist");
+    }
+
+    println!("{}", args.input_file_path);
+
+    let mut t = Transformer::new(&args.input_file_path);
+    let _ = t.transform();
 }
