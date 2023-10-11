de0789a77a1f1d66374ddee3c5f0eceeba5f76fb
Ben Bass <benbass@codedstructure.net>
2023-10-11 22:20:19 +0100

    basic integration tests for simple geometry

---
+++ tests/integration_tests.rs
@@ -0,0 +1,53 @@
+use std::io::Cursor;
+use svgd::Transformer;
+
+fn compare(input: &str, expected: &str) {
+    let mut t = Transformer::new();
+    let mut output: Vec<u8> = vec![];
+
+    let mut input = Cursor::new(input);
+    t.transform(&mut input, &mut output);
+    let output = String::from_utf8(output).unwrap();
+
+    assert_eq!(output, expected);
+}
+
+#[test]
+fn test_expand_rect_xy_wh() {
+    let input = r#"<svg><rect xy="1 2" wh="3 4"/></svg>"#;
+    let expected = r#"<svg><rect x="1" y="2" width="3" height="4"/></svg>"#;
+
+    compare(input, expected);
+}
+
+#[test]
+fn test_expand_xy1_xy2() {
+    let input = r#"<svg><line xy1="1 2" xy2="3 4"/></svg>"#;
+    let expected = r#"<svg><line x1="1" y1="2" x2="3" y2="4"/></svg>"#;
+
+    compare(input, expected);
+}
+
+#[test]
+fn test_expand_rect_xy1_wh() {
+    let input = r#"<svg><rect xy1="1 2" wh="3 6"/></svg>"#;
+    let expected = r#"<svg><rect x="1" y="2" width="3" height="6"/></svg>"#;
+
+    compare(input, expected);
+}
+
+#[test]
+fn test_expand_rect_xy1_xy2() {
+    let input = r#"<svg><rect xy1="1 2" xy2="3 6"/></svg>"#;
+    let expected = r#"<svg><rect x="1" y="2" width="2" height="4"/></svg>"#;
+
+    compare(input, expected);
+}
+
+#[test]
+fn test_expand_rect_wh_xy2() {
+    let input = r#"<svg><rect xy2="4 6" wh="2 1"/></svg>"#;
+    let expected = r#"<svg><rect x="2" y="5" width="2" height="1"/></svg>"#;
+
+    compare(input, expected);
+}
