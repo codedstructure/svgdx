10cdaf4095cdd40110edcca64ee6966d83c16e69
Ben Bass <benbass@codedstructure.net>
2024-12-31 19:55:45 +0000

    fixup

--- src/transform.rs
+++ src/transform.rs
@@ -140,7 +140,7 @@ impl EventGen for Container {
                 }
 
                 if bbox.is_some() {
-                    context.set_prev_element(new_el);
+                    context.set_prev_element(&new_el);
                 }
                 Ok((events, bbox))
             }
